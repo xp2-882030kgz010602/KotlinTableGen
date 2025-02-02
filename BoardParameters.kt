58a986c9567abfded84d906e37c2ebabe40c02be
xp2-882030kgz010602 <38965142+xp2-882030kgz010602@users.noreply.github.com>
2025-02-01 21:20:08 -0500

    Delete BoardParameters.kt

--- BoardParameters.kt
+++
@@ -1,5 +0,0 @@
-/** An unencapsulated class storing parameters that specify board size and movement. */
-class BoardParameters(val boardSize:Int,val movesMovable:Array<Array<IntArray>>,val movesImmovable:Array<Array<IntArray>>)
-  //If `moveNames[i]` is the name of a given move,
-  //then `movesMovable[i]` describes which columns can move,
-  //and `movesImmovable[i]` describes which columns can't.
